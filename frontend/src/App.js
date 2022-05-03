96949cb6c4cc83039be94ca7478d03b1579ccd65
fafa-a <fabienpuech1@gmail.com>
2022-05-03 13:21:08 +0200

    feat: add Loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,49 @@
 import { useState, useEffect } from "react"
 import "./App.css"
 import { getAllStudents } from "./client"
-import { Table } from "antd"
+import { Avatar, Spin, Table } from "antd"
+import { FontSizeOutlined, LoadingOutlined } from "@ant-design/icons"
+
 import Container from "./Container"
 
 function App() {
   const [students, setStudents] = useState([])
+  const [isFetching, setIsFetching] = useState(false)
 
   useEffect(() => {
     fetchStudents()
   }, [])
 
   const fetchStudents = () => {
+    setIsFetching(true)
     getAllStudents()
       .then(res => res.json())
       .then(students => {
         setStudents(students)
+        setIsFetching(false)
       })
   }
 
+  if (isFetching) {
+    return (
+      <Container>
+        <LoadingOutlined style={{ fontSize: 24 }} />
+      </Container>
+    )
+  }
   if (students?.length) {
     return students.map((student, id) => {
       const columns = [
+        {
+          title: "",
+          key: "Avatar",
+          render: (text, student) => (
+            <Avatar size="large">
+              {`${student.firstName.charAt(0).toUpperCase()}
+                ${student.lastName.charAt(0).toUpperCase()}`}
+            </Avatar>
+          ),
+        },
         { title: "Student ID", dataIndex: "studentID", key: "studentID" },
         { title: "First Name", dataIndex: "firstName", key: "firstName" },
         { title: "Last Name", dataIndex: "lastName", key: "lastName" },
@@ -29,7 +51,7 @@ function App() {
         { title: "Gender", dataIndex: "gender", key: "gender" },
       ]
       return (
-        <Container>
+        <Container key={id}>
           <Table
             dataSource={students}
             columns={columns}
--- frontend/src/Container.jsx
+++ frontend/src/Container.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 const Container = props => {
   return (
-    <div style={{ width: "1440px", margin: "0 auto" }}>{props.children}</div>
+    <div style={{ width: "1440px", margin: "0 auto", textAlign: "center" }}>
+      {props.children}
+    </div>
   )
 }
 export default Container
