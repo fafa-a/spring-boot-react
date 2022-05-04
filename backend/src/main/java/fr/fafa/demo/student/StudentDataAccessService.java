02e3baf6cd598ec2ee94165d41c4b377f7e004d6
fafa-a <fabienpuech1@gmail.com>
2022-05-04 11:04:42 +0200

    feat: add student mapper in accessService

--- backend/src/main/java/fr/fafa/demo/student/StudentDataAccessService.java
+++ backend/src/main/java/fr/fafa/demo/student/StudentDataAccessService.java
@@ -3,15 +3,31 @@ package fr.fafa.demo.student;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentDataAccessService {
 
-  public List<Student> selectAllStudents() {
+  private final JdbcTemplate jdbcTemplate;
 
-    return List.of(
-        new Student(UUID.randomUUID(), "Cyvian", "Him", "cycy@mail.com", Student.Gender.FEMALE),
-        new Student(UUID.randomUUID(), "Chris", "Burckard", "chris@mail.com", Student.Gender.MALE));
+  @Autowired
+  public StudentDataAccessService(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  public List<Student> selectAllStudents() {
+    String sql = "SELECT student_id, first_name, last_name, email, gender FROM student";
+    return jdbcTemplate.query(sql, (resultSet, i) -> {
+      String studentIdstr = resultSet.getString("student_id");
+      UUID studentId = UUID.fromString(studentIdstr);
+      String firstName = resultSet.getString("first_name");
+      String lastName = resultSet.getString("last_name");
+      String email = resultSet.getString("email");
+      String genderStr = resultSet.getString("gender");
+      Student.Gender gender = Student.Gender.valueOf(genderStr.toUpperCase());
+      return new Student(studentId, firstName, lastName, email, gender);
+    });
   }
 }
