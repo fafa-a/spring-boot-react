7003b0106d8fcdd8af6ec218307109391d8ce73d
fafa-a <fabienpuech1@gmail.com>
2022-05-04 15:35:44 +0200

    feat: post mapping

--- backend/src/main/java/fr/fafa/demo/student/Student.java
+++ backend/src/main/java/fr/fafa/demo/student/Student.java
@@ -2,17 +2,26 @@ package fr.fafa.demo.student;
 
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class Student {
+
+  @JsonProperty("studentID")
   private final UUID studentID;
+  @JsonProperty("firstName")
   private final String firstName;
+  @JsonProperty("lastName")
   private final String lastName;
+  @JsonProperty("email")
   private final String email;
+  @JsonProperty("gender")
   private final Gender gender;
 
   public enum Gender {
     MALE, FEMALE
   }
+
 }
--- backend/src/main/java/fr/fafa/demo/student/StudentController.java
+++ backend/src/main/java/fr/fafa/demo/student/StudentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +22,10 @@ public class StudentController {
   @GetMapping("/students")
   public List<Student> getAllStudents() {
     return studentService.getAllStudents();
+  }
+
+  @PostMapping("/students")
+  public void addNewStudent(@RequestBody Student student) {
+    System.out.println(student);
   }
 }
