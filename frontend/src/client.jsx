54a6c19358a6ae8728132b6a368c4c2faa2ddddb
fafa-a <fabienpuech1@gmail.com>
2022-05-04 15:41:53 +0200

    feat: addNewStudent method

--- frontend/src/client.jsx
+++ frontend/src/client.jsx
@@ -1,3 +1,9 @@
 import fetch from "unfetch"
 
 export const getAllStudents = () => fetch("/students")
+export const addNewStudents = student =>
+  fetch("/students", {
+    header: { "Content-Type": "application/json" },
+    method: "POST",
+    body: JSON.stringify(student),
+  })
